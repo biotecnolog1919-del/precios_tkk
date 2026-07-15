96eff39c369ebab70f756ff2bdd2d5541504d24d
Daniel <biotecnolog1919@gmail.com>
2026-07-14 18:27:51 -0600

    Añadimos while

--- precios.py
+++ precios.py
@@ -13,11 +13,12 @@ def primera_fase_comparar(precio_base):
     if precio_base >= 500:
         return up_ganancia(precio_base)
     else:
-        return down_ganancia(precio_base)
-
-
-precio_base = int(input("Ingresa el precio base: "))
-resultado = primera_fase_comparar(precio_base)
-print(f"Precio TTI: ${resultado:.2f}")    
+        return down_ganancia(precio_base)   
 
    
+while True:  
+  precio_base = int(input("Ingresa el precio base: "))
+  resultado = primera_fase_comparar(precio_base)
+  print(f"Precio TTI: ${resultado:.2f}") 
+
+  
